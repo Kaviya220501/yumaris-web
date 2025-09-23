fed8079c8d336cc5c5a5dd8bdc8a512f0b55e863
pooja020105 <prpooja418@gmail.com>
2025-09-23 18:36:46 +0530

    nav bar

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import NavBar  from "../Layout/NavBar"
+const Home=() => {
+  return (
+<div className="h-screen w-screen bg-black text-white">
+      <NavBar />
+   
+
+
+       </div>
+  )
+}
+export default Home
