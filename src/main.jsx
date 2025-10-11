2663e9ea141426f70a0302245371a055dc0c7443
pooja020105 <prpooja418@gmail.com>
2025-10-11 15:33:16 +0530

    home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from './Components/Home.jsx';
-
+import About from './Components/About.jsx';
+import Services from './Components/Services.jsx';
+import Contact from './Components/Contact.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+);
